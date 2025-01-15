e68bc0a9829cbb454c843dc60685ddebab4baa87
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-15 21:59:02 +0530

    built header and add netflix backaground login image and also setup route using react router dom

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_small.jpg" alt="Netfliximage"/>
+      </div>
+     
+    </div>
+  )
+}
+
+export default Login
