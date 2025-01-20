8ced5a658df8d8e5dec8f61666d899a7a8730d44
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-20 22:24:37 +0530

    unsubscribe to the onAuthChanged callback

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
 
 
   useEffect(()=> {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe =  onAuthStateChanged(auth, (user) => {
         if (user) {
           const {uid , email , displayName, photoURL} = user;
           dispatch(addUser({uid : uid, email : email, displayName : displayName , photoURL : photoURL}));
@@ -34,6 +34,7 @@ const Header = () => {
           navigate("/")
         }
       });
+      return () => unsubscribe();
 },[dispatch, navigate]);
   return (
     <div className='absolute w-screen px-32 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
