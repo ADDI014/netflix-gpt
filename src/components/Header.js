9201d393a52d946f91469638da3151508d5f0eab
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-20 22:05:03 +0530

    Bug fix: if the user is not logged in redirect /browsse to login page and vice versa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { auth } from '../utils/firebase';
-import { signOut } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
+
  
   const handleSignOut = () => {
     signOut(auth).then(() => {
       // Sign-out successful.
-      navigate("/");
     }).catch((error) => {
 
       // An error happened.
       navigate("/error");
     });
   }
+
+
+  useEffect(()=> {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid , email , displayName, photoURL} = user;
+          dispatch(addUser({uid : uid, email : email, displayName : displayName , photoURL : photoURL}));
+          navigate("/browse");
+        } else {
+          // User is signed out
+          dispatch(removeUser());
+          navigate("/")
+        }
+      });
+},[dispatch, navigate]);
   return (
     <div className='absolute w-screen px-32 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-48' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,13 @@ import Header from './Header'
 import {checkValidData} from "../utils/validate";
 import {createUserWithEmailAndPassword , signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {auth} from "../utils/firebase";
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 const Login = () => {
 
   const [isSignInForm , setIsSignInForm] = useState(true);
   const [errorMessage , setErrorMessage] = useState(null);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const name = useRef(null);
@@ -44,7 +43,7 @@ const Login = () => {
       // Profile updated!
       const {uid , email , displayName, photoURL} = auth.currentUser;
       dispatch(addUser({uid : uid, email : email, displayName : displayName , photoURL : photoURL}));
-      navigate("/browse");
+      // navigate("/browse");
     }).catch((error) => {
       // An error occurred
       setErrorMessage(error.message);
@@ -63,8 +62,8 @@ const Login = () => {
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
-    navigate("/browse");
+    // console.log(user);
+    // navigate("/browse");
   })
   .catch((error) => {
     const errorCode = error.code;
