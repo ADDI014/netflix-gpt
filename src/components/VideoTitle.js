6622d227790a7e761e7cda67272e7c6834d94978
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-22 22:49:22 +0530

    built video title and overview

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title , overview}) => {
   return (
-    <div>
-      VideoTitle
+    <div className='pt-36 px-12'>
+      <h1 className='text-5xl font-bold'>{title}</h1>
+      <p className='py-6 text-lg w-1/2'>{overview}</p>
+      <div className=''>
+        <button className='bg-gray-500 text-white p-4 px-6 text-2xl bg-opacity-50 rounded-lg'>▶ Play</button>
+        <button className='bg-gray-500 mx-2 text-white p-4 px-6 text-2xl bg-opacity-50 rounded-lg'>More Info</button>
+      </div>
     </div>
   )
 }
