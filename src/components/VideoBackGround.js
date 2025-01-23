6f4e56fcbaccf739c4ef8bafcf1c842a3d000cfc
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-23 20:27:10 +0530

    we dont need useState to manage the trailer video we have used redux to store the trailerVideo

--- src/components/VideoBackGround.js
+++ src/components/VideoBackGround.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { API_OPTIONS } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux';
+import { addTrailerVideo } from '../utils/moviesSlice';
 
 const VideoBackGround = ({movieId}) => {
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+    const dispatch = useDispatch();
 
-    const [trailerId , setTrailerId] = useState(null);
+    // const [trailerId , setTrailerId] = useState(null);
 
     const getMoviesVideos = async () => {
         const data = await fetch("https://api.themoviedb.org/3/movie/976573/videos?language=en-US", API_OPTIONS);
@@ -13,7 +17,8 @@ const VideoBackGround = ({movieId}) => {
         const filteredData = json.results.filter((video) => video.type === "Trailer");
         const trailer = filteredData.length ? filteredData[0] : json.results[0];
         console.log(trailer);
-        setTrailerId(trailer.key);
+        // setTrailerId(trailer.key);
+        dispatch(addTrailerVideo(trailer))
     }
 
     useEffect(()=>{
@@ -22,7 +27,7 @@ const VideoBackGround = ({movieId}) => {
 
   return (
     <div>
-      <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerId} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+      <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerVideo?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
     </div>
   )
 }
